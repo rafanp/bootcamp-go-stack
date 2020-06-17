92c81b5067bd439daa1496dcb355795dc28f09b2
rafanp <raasnp@gmail.com>
2020-06-17 15:49:48 -0300

    rotas

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const { request, response } = require('express');
 
 const app = express();
 
-app.get('/',(request,response) => {
-    return response.json({ message: 'Hello World'});
+app.get('/projects', (request,response) => {
+    return response.json({ message: 'Hello'});
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('Back-end started! :D');
+});
